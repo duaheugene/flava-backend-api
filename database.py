73cbbea2f1de7209809fa1ec17e23f0d4ecb4bc8
aessein18 <angelaessein@gmail.com>
2026-05-20 22:31:17 +0000

    Updated database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+DATABASE_URL = "sqlite:///./flava.db"
+
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
